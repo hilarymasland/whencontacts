64adb9bbf25fc5770f489ca026b4efc2d54badf4
Hilary Masland <hilarymasland@gmail.com>
2016-04-14 10:54:01 -0400

    added express.static and css

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ var app     = express();
 
 app.set("view engine", "hbs");
 app.engine(".hbs", hbs({
-  extname: ".hbs",
-  partialsDir: "views/",
-  layoutsDir: "views/",
-  defaultLayout: "layout-main"
+  extname:        ".hbs",
+  partialsDir:    "views/",
+  layoutsDir:     "views/",
+  defaultLayout:  "layout-main"
 }));
+app.use("/assets", express.static("public"));
 
 app.get("/", function(req, res){
   res.render("app-welcome");

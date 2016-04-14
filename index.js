8ee47c6f78b508fc40d848cf4f96b71bd63315c0
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-04-14 11:29:15 -0400

    Add Students Show route

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var hbs     = require("express-handlebars");
 var db      = require("./db/connection");
 var app     = express();
 
-
-
 app.set("view engine", "hbs");
 app.engine(".hbs", hbs({
   extname:        ".hbs",
@@ -12,6 +10,7 @@ app.engine(".hbs", hbs({
   layoutsDir:     "views/",
   defaultLayout:  "layout-main"
 }));
+
 app.use("/assets", express.static("public"));
 
 app.get("/", function(req, res){
@@ -21,6 +20,17 @@ app.get("/", function(req, res){
 app.get("/students", function(req, res){
   res.render("students-index", {
     students: db.students
+  });
+});
+
+app.get("/students/:name", function(req, res){
+  var data = {
+    name: req.params.name,
+    phone_number: req.params.phone_number,
+    email_address: req.params.email_address
+  }
+  res.render("students-show", {
+    student: data
   });
 });
 

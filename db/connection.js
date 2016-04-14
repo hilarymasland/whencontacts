bd8534444a8d4acdb1e7c9dc50b91bd2881e3487
Hilary Masland <hilarymasland@gmail.com>
2016-04-14 15:56:43 -0400

    db. connection mongoose

--- db/connection.js
+++ db/connection.js
@@ -9,5 +9,10 @@ var StudentSchema = new mongoose.Schema(
 );
 mongoose.model("Student", StudentSchema);
 mongoose.connect("mongodb://localhost/whencontacts");
+if(process.env.NODE_ENV == "production"){
+  mongoose.connect(process.env.MONGOLAB_URL);
+}else{
+  mongoose.connect("mongodb://localhost/whencontacts");
+}
 
 module.exports = mongoose;

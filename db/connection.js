797d809fd449396b354a6404a6474e7b9f6f4254
Hilary Masland <hilarymasland@gmail.com>
2016-04-14 17:13:54 -0400

    more debugs

--- db/connection.js
+++ db/connection.js
@@ -11,7 +11,7 @@ mongoose.model("Student", StudentSchema);
 // mongoose.connect(process.env.MONGODB_URI);
 
 if(process.env.NODE_ENV == "production"){
-  mongoose.connect(process.env.MONGODB_URL);
+  mongoose.connect(process.env.MONGODB_URI);
 }else{
   mongoose.connect("mongodb://localhost/whencontacts");
 }

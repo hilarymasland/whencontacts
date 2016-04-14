1d154aee5267896c22266d7bffefcbb6e7aa245f
Hilary Masland <hilarymasland@gmail.com>
2016-04-14 17:06:42 -0400

    error debugging

--- db/connection.js
+++ db/connection.js
@@ -9,10 +9,10 @@ var StudentSchema = new mongoose.Schema(
 );
 mongoose.model("Student", StudentSchema);
 
-if(process.env.NODE_ENV == "production"){
-  mongoose.connect(process.env.MONGODB_URL);
-}else{
-  mongoose.connect("mongodb://localhost/whencontacts");
-}
+// if(process.env.NODE_ENV == "production"){
+//   mongoose.connect(process.env.MONGODB_URL);
+// }else{
+//   mongoose.connect("mongodb://localhost/whencontacts");
+// }
 
 module.exports = mongoose;

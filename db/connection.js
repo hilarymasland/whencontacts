cc74fcc60bf0b28cd000d0a41f98e01f2cf7424d
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-14 12:17:08 -0400

    added mongoose connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,15 @@
+var mongoose = require("mongoose");
+
+var StudentSchema = new mongoose.Schema(
+  {
+    name: String,
+    phone: String,
+    email: String,
+  }
+);
+mongoose.model("Student", StudentSchema);
+mongoose.connect("mongodb://localhost/whencontact");
+
 var seedData = require("./seeds.json");
 module.exports = {
   students: seedData
